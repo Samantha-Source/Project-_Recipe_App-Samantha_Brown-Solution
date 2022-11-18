d6f0d6aec7584d25a0a83d33fc32828e99ed6720
Samantha Brown <S.BrownEMS@gmail.com>
2022-11-18 15:24:01 -0500

    Updates

---
+++ precache-manifest.1f3bb40290a7bc4ce173dc657d97d00b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f785fc903f5ed7a079f754cc6842383",
+    "url": "/Recipe_App/index.html"
+  },
+  {
+    "revision": "459533efffece3fa807b",
+    "url": "/Recipe_App/static/css/main.28d6758f.chunk.css"
+  },
+  {
+    "revision": "1835edcf0698c4cc1363",
+    "url": "/Recipe_App/static/js/2.9de04983.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Recipe_App/static/js/2.9de04983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459533efffece3fa807b",
+    "url": "/Recipe_App/static/js/main.a003e3dc.chunk.js"
+  },
+  {
+    "revision": "720802c55f4a9a1ffa83",
+    "url": "/Recipe_App/static/js/runtime-main.62b0a021.js"
+  }
+]);
